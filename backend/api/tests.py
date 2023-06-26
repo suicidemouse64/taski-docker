8f127474b9fc574640a2345ae60db93b36c4acd2
Виталий Красильников <suicidemouse164@mail.ru>
2023-06-26 22:51:50 +0300

    Actions tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -3,11 +3,13 @@ from http import HTTPStatus
 from api import models
 from django.test import Client, TestCase
 
+
 class TaskiAPITestCase(TestCase):
+
     def setUp(self):
         self.guest_client = Client()
 
-   def test_list_exists(self):
+    def test_list_exists(self):
         """Проверка доступности списка задач."""
         response = self.guest_client.get('/api/tasks/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
